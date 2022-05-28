e763f46edb3b77d55dcfb51a66c5ee6c41a260f0
mnmustafa1109 <mnmustafa1109@gmail.com>
2022-05-28 14:40:09 +0500

    added texture

--- src/main.cxx
+++ src/main.cxx
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <cmath>
 #include <iostream>
+#include "../include/stb_image.h"
 
 #include "../include/shader.hxx"
 
@@ -11,7 +12,6 @@ void processInput(GLFWwindow* window);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
 
 int main() {
     // glfw: initialize and configure
@@ -55,35 +55,77 @@ int main() {
     // we skipped compile log checks this time for readability (if you do
     // encounter issues, add the compile-checks! see previous code samples)
 
-    int success;
-    char infoLog[512];
-
-    Shader shaderProgram1 = Shader("../shaders/shader1.vs", "../shaders/shader1.fs");
-    Shader shaderProgram2 = Shader("../shaders/shader2.vs", "../shaders/shader2.fs");
+    Shader shaderProgram1 =
+        Shader("../shaders/shader1.vs", "../shaders/shader1.fs");
+    Shader shaderProgram2 =
+        Shader("../shaders/shader2.vs", "../shaders/shader2.fs");
+    Shader shaderProgram3 =
+        Shader("../shaders/shader3.vs", "../shaders/shader3.fs");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float firstTriangle[] = {
-        0.0f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
-        -0.9f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom left
-        -0.45f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f   // top
+        0.0f,   -0.8f, 0.0f, 0.0f, 1.0f, 1.0f,  // bottom right
+        -0.9f,  -0.8f, 0.0f, 1.0f, 1.0f, 0.0f,  // bottom left
+        -0.45f, 0.2f,  0.0f, 1.0f, 0.0f, 1.0f   // top
     };
     float secondTriangle[] = {
-        0.0f,  -0.5f, 0.0f,  // left
-        0.9f,  -0.5f, 0.0f,  // right
-        0.45f, 0.5f,  0.0f   // top
+        0.0f,  -0.8f, 0.0f,  // left
+        0.9f,  -0.8f, 0.0f,  // right
+        0.45f, 0.2f,  0.0f   // top
     };
     float rectangle[] = {
-        0.8f, 0.8f, 0.0f,  // top right
-        0.8f, 0.6f, 0.0f,  // bottom right
-        0.6f, 0.6f, 0.0f,  // bottom left
-        0.6f, 0.8f, 0.0f   // top left
-    };
+        0.9f,   0.9f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
+        0.9f,   0.4f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,   // bottom right
+        -0.45f, 0.4f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // bottom left
+        -0.45f, 0.9f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f};  // top left
     unsigned int indices[] = {
         // note that we start from 0!
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
     };
+
+    unsigned int textures[2];
+    glGenTextures(2, textures);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);
+    // set the textures[0] wrapping/filtering options (on currently bound
+    // textures[0])
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the textures[0]
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("../data/textures/brickes.jpg", &width,
+                                    &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                     GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load textures[0]" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+    // set the textures[1] wrapping/filtering options (on currently bound
+    // textures[1])
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the textures[1]
+    data = stbi_load("../data/textures/flags.png", &width, &height,
+                     &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA,
+                     GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load textures[1]" << std::endl;
+    }
+    stbi_image_free(data);
+
 
     unsigned int VBOs[3], VAOs[3], EBO;
     glGenVertexArrays(3, VAOs);  // we can also generate multiple VAOs or
@@ -130,9 +172,19 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                  GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
                           (void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                          (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    shaderProgram3.use();
+    shaderProgram3.set("texture1", 0);
+    shaderProgram3.set("texture2", 1); // or with shader clas
 
     // render loop
     // -----------
@@ -165,8 +217,11 @@ int main() {
         // this call should output a yellow triangle
         // then we draw the rectangle using the data from our second VAO
 
-        shaderProgram2.use();
-        shaderProgram2.setv4f("ourColor", 1.0f, 0.0f, 0.0f, 1.0f);
+        shaderProgram3.use();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, textures[1]);
         glBindVertexArray(VAOs[2]);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -195,6 +250,7 @@ int main() {
 void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback
